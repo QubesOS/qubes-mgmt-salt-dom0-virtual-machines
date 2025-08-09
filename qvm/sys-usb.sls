QubesOS/qubes-mgmt-salt-dom0-virtual-machines

commit 784e5963787ce443c91b6f969f7671c8064063b6

Prioritize sys-usb autostart in qvm.usb-keyboard, not in qvm.sys-usb

qvm/sys-usb.sls
@@ -35,7 +35,6 @@ include:
   - qvm.default-dispvm
   {% endif %}
   - qvm.hide-usb-from-dom0
-  - qvm.sys-usb-prioritize-autostart
 
 {% from "qvm/template.jinja" import load -%}
 

qvm/usb-keyboard.sls
@@ -18,6 +18,7 @@
 
 include:
  - qvm.sys-usb
+ - qvm.sys-usb-prioritize-autostart
 
 {% from "qvm/template.jinja" import load -%}
 

commit d58d769ccf664b23b7adfade566b6a77503c08e5

Prioritize sys-usb startup also when usb keyboard is enabled via pillar
Initial setup enables USB keyboard via pillar, to be configured when initially
setting up sys-usb. Prioritize sys-usb startup in this case too.

Fixes: 784e596 "Prioritize sys-usb autostart in qvm.usb-keyboard, not in qvm.sys-usb"

qvm/sys-usb.sls
@@ -35,6 +35,9 @@ include:
   - qvm.default-dispvm
   {% endif %}
   - qvm.hide-usb-from-dom0
+  {% if salt['pillar.get']('qvm:sys-usb:keyboard-action', 'deny') == 'allow' %}
+  - qvm.sys-usb-prioritize-autostart
+  {% endif %}
 
 {% from "qvm/template.jinja" import load -%}
 